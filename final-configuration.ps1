04bccbe27fa438e62a7d03f56417ae9b101fbdee
Benjamin Hodge <bhodge@fiarro.com>
2016-11-14 06:28:31 +1100

    Modified install to account for W2012R2 having legacy New-SelfSignedCertificate

--- final-configuration.ps1
+++ final-configuration.ps1
@@ -1,3 +1,5 @@
+
+
 # Copy the CloudBot scripts into the Hubot Scripts folder
 $cloudbotFiles = Get-ChildItem C:\windows\temp\cloudbot\cloudbot-master
 
@@ -15,7 +17,9 @@ $credential = Get-Credential
 
 Add-AzureRmAccount -Credential $credential -SubscriptionName $subscriptionName
 
-$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\LocalMachine\My" -DnsName "cloudbot"
+Import-Module .\New-SelfSignedCertificateEx.ps1
+$cert = New-SelfSignedCertificateEx -Subject "CN=cloudbotCert" -KeySpec "Exchange" -FriendlyName "cloudbotCert"
+
 $keyValue = [System.Convert]::ToBase64String($cert.GetRawCertData())
 $app = New-AzureRmADApplication -DisplayName "CloudBot" -HomePage "https://github.com/autonomousit/cloudbot" -IdentifierUris "https://github.com/autonomousit/cloudbot" -CertValue $keyValue -EndDate $cert.NotAfter -StartDate $cert.NotBefore
 
@@ -25,4 +29,4 @@ New-AzureRmRoleAssignment -RoleDefinitionName Contributor -ServicePrincipalName 
 
 $tenant = (Get-AzureRmSubscription -SubscriptionName $subscriptionName).TenantId
 
-@{"thumprint" = $cert.Thumbprint; "applicationId" = $app.ApplicationId; "tenantID" = $tenant} | ConvertTo-Json > "C:\cloudbot\scripts\config\serviceprincipals\azure.json"
+@{"thumbprint" = $cert.Thumbprint; "applicationId" = $app.ApplicationId; "tenantID" = $tenant} | ConvertTo-Json > "C:\cloudbot\scripts\config\serviceprincipals\azure.json"
