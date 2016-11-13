b2fc90f285d9ef900b5a39af054c9949c622c530
Benjamin Hodge <bhodge@fiarro.com>
2016-11-13 16:52:53 +1100

    Added final configuration script

---
+++ final-configuration.ps1
@@ -0,0 +1,28 @@
+# Copy the CloudBot scripts into the Hubot Scripts folder
+$cloudbotFiles = Get-ChildItem C:\windows\temp\cloudbot\cloudbot-master
+
+foreach ($item in $cloudbotFiles) {
+
+    Copy-Item -Path $item.FullName -Destination c:\cloudbot\scripts -Recurse -Container
+}
+
+# Restart the Hubot Service
+Get-Service *Hubot* | Restart-Service
+
+# Create an Azure Service Principal for CloudBot authentication to Azure
+$subscriptionName = Read-Host -Prompt "Enter your Azure Subscription Name"
+$credential = Get-Credential
+
+Add-AzureRmAccount -Credential $credential -SubscriptionName $subscriptionName
+
+$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\LocalMachine\My" -DnsName "cloudbot"
+$keyValue = [System.Convert]::ToBase64String($cert.GetRawCertData())
+$app = New-AzureRmADApplication -DisplayName "CloudBot" -HomePage "https://github.com/autonomousit/cloudbot" -IdentifierUris "https://github.com/autonomousit/cloudbot" -CertValue $keyValue -EndDate $cert.NotAfter -StartDate $cert.NotBefore
+
+New-AzureRmADServicePrincipal -ApplicationId $app.ApplicationId
+
+New-AzureRmRoleAssignment -RoleDefinitionName Contributor -ServicePrincipalName $app.ApplicationId.Guid
+
+$tenant = (Get-AzureRmSubscription -SubscriptionName $subscriptionName).TenantId
+
+@{"thumprint" = $cert.Thumbprint; "applicationId" = $app.ApplicationId; "tenantID" = $tenant} | ConvertTo-Json > "C:\cloudbot\scripts\config\serviceprincipals\azure.json"
